f6e4aac7fb736f3dbb75a719857b81f1c2deba2c
Steven A. Matos <steven_matos@ymail.com>
2025-01-12 14:19:04 -0500

    main page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,55 @@
 import React from "react";
 import "../App.css";
+import couple from "../assets/olga-keith.jpg";
 
 const Header = () => {
   return (
     <>
       <header className="header">
-        <h1 className="title style-script-regular">Keith & Olga 2025</h1>
+        <h1 className="title style-script-regular">Keith & Olga</h1>
       </header>
       <section>
-        <article className="text-center p-2">
-          <h2 className="text-2xl py-1">
-            Welcome to Olga & Keith's Wedding Website!
-          </h2>
+        <img
+          src={couple}
+          className="m-auto mb-2 h-96 rounded-md"
+          alt="Olga and Keith"
+        />
+        <article className="text-center p-3">
           <p>
-            As we count down to our special day on May 2025, we're excited to
-            share this celebration with you. Share your favorite photos from the
-            wedding day with us and relive the magic! <br />
-            <br /> Upload your pics here and let's create a treasure trove of
-            memories together!
+            As we prepare for our special wedding day on <b>May 17, 2025</b>,
+            from 4pm to 10pm, we're thrilled to share this special moment with
+            you.
+            <br />
+            <br /> Join us as we celebrate the union of Olga and Keith in an
+            evening filled with love, joy, and cherished memories!
+            <br />
+            <br />
+          </p>
+          <p>
+            <b>RSVP by March 25</b>
+            <br />
+            <a className="underline underline-offset-4" href="tel:321-437-8105">
+              321-437-8105
+            </a>
+          </p>
+          <br />
+          <address>
+            <span className="italic">Address</span>
+            <br />
+            <a
+              className="underline underline-offset-4"
+              href="https://maps.app.goo.gl/vLxoRTqfQmMBrh597"
+              rel="noreferrer noopener"
+              target="_blank"
+            >
+              414 King St, Ovideo, FL 32765
+            </a>
+          </address>
+          <p>
+            <br />
+            Share your favorite photos from the wedding day with us and relive
+            the magic! Upload your pics below and let's create a treasure trove
+            of memories together!
           </p>
         </article>
       </section>
