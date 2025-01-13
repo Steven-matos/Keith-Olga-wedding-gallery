73e378550ecd2632f2d6d267f04bdeba4f06eebb
Steven A. Matos <steven_matos@ymail.com>
2025-01-12 19:21:12 -0500

    update index

--- project/backend/api/index.js
+++ project/backend/api/index.js
@@ -1,25 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
 dotenv.config();
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
+// Define a route handler for the root URL ("/")
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Connected to the server" });
+  res.status(404).send("Root page not found!");
 });
 
 const uploadRoutes = require("./upload");
 const photoRoutes = require("./photo");
 
+// Use the routes
 app.use("/api/upload", uploadRoutes);
 app.use("/api/photo", photoRoutes);
 
