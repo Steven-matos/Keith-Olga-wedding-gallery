ad83783d26f242d83446e7f53bbae6f966ae15b4
Steven A. Matos <steven_matos@ymail.com>
2025-01-12 19:43:14 -0500

    update on server

--- project/backend/api/index.js
+++ project/backend/api/index.js
@@ -2,19 +2,31 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+const cors = require("cors"); // Add this line
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors()); // Use the cors middleware
+
+// Set allowed origins, methods and headers here
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-// Define a route handler for the root URL ("/")
 app.get("/", (req, res) => {
-  res.status(404).send("Root page not found!");
+  res.status(200).send("Connected to Server");
 });
-
 const uploadRoutes = require("./upload");
 const photoRoutes = require("./photo");
 
